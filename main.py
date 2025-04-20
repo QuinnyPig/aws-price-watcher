47be7f45ed8187f3f04093932d10826fa87b8726
Ian Mckay <contact@ianmckay.com.au>
2025-04-20 22:50:25 +1000

    Add comparison and workflow

--- main.py
+++ main.py
@@ -17,9 +17,21 @@ def save_file(filename, contents):
     except IOError as e:
         print(f"Error saving the file: {e}")
 
+def load_file(filename):
+    existing_contents = None
+    try:
+        existing_file = open(f"raw/{sanitized_service_name}.json", "r")
+        existing_contents = existing_file.read()
+        existing_file.close()
+    except FileNotFoundError:
+        existing_contents = None
+    return existing_contents
+
 
 services = []
 not_found = []
+modified_services = []
+new_services = []
 
 service_list_obj = {}
 
@@ -35,8 +47,22 @@ if service_list:
         sanitized_service_name = service.lower().removeprefix("amazon").removeprefix("aws")
         contents = get_url_contents(f"https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/{sanitized_service_name}/USD/current/{sanitized_service_name}.json")
         if contents:
+            contents_obj = json.loads(contents)
+            existing_contents = load_file(sanitized_service_name)
+            if existing_contents:
+                existing_contents_obj = json.loads(existing_contents)
+
+                # compare the two objects
+                if existing_contents_obj != contents_obj:
+                    modified_services.append(sanitized_service_name)
+            else:
+                new_services.append(sanitized_service_name)
+            
             save_file(f"raw/{sanitized_service_name}.json", contents)
         else:
             not_found.append(sanitized_service_name)
-            print(f"Service {sanitized_service_name} not found.")
 
+    print("## Latest Changes\n\n")
+    print(f"**New services:**\n\n- {'\n- '.join(new_services)}")
+    print(f"\n**Modified services:**\n\n- {'\n- '.join(modified_services)}")
+    print(f"\n**Not found services:**\n\n- {'\n- '.join(not_found)}")
